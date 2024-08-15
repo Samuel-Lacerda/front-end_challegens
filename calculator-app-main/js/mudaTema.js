106a3bfdeb332da85249727058c487c0b181529e
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-08-15 06:48:34 -0300

    update

--- calculator-app-main/js/mudaTema.js
+++ calculator-app-main/js/mudaTema.js
@@ -1,14 +1,27 @@
 const botaoMudaTema = document.querySelector('#input-tema')
-let contadorDeClicks = 1
-botaoMudaTema.addEventListener('click',mudarTema)
-console.log(botaoMudaTema)
-function mudarTema(){
+const fundo = document.body
+const telaFundo = document.querySelector('.calculadora__tela')
+const fundoAreaTeclas = document.querySelector('.calculadora__teclas')
+const calcNome = document.querySelector('.calc')
+const AreaTemas = document.querySelector('.temas')
 
-    botaoMudaTema.value = contadorDeClicks
-    console.log(botaoMudaTema.value)
-    if(contadorDeClicks < 4 && contadorDeClicks > 0){
-        contadorDeClicks ++
-    } else {
-        contadorDeClicks = 1
+let contadorDeClicks = 1
+
+
+botaoMudaTema.addEventListener('input', (event) => {
+    const valorAtual = event.target.value
+
+    if(valorAtual == 1){
+        calcNome.style.color = 'default'
     }
-}
+
+    if (valorAtual == 2) {
+        calcNome.style.color = 'var(--dark-grayish-blue-01)'
+        AreaTemas.style.color = 'var(--dark-grayish-blue-01)'
+        fundo.style.background = 'var(--main-background-02)'
+        telaFundo.style.background = 'var(--screen-background-02)'
+        telaFundo.style.color = 'var(--dark-grayish-blue-01)'
+        fundoAreaTeclas.style.background = 'var(--keypad-background-02)'
+        botaoMudaTema.style.background = 'var(--keypad-background-02)'
+    }
+})
