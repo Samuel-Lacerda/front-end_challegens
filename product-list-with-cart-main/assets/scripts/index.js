8a38d3fa37254120700cb4e4523bdc6733257442
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2024-10-05 11:52:47 -0300

    ajustes css

--- product-list-with-cart-main/assets/scripts/index.js
+++ product-list-with-cart-main/assets/scripts/index.js
@@ -1,4 +1,7 @@
 const nomeItem = document.querySelectorAll('.menu__item__name')
+const itensNoCarrinho = document.querySelector('.cart__num')
+const textoCarrinhoVazio = document.querySelector('.cart-empyt')
+
 itensMenu = []
 
 //Pega cada item já existente na pagina, remove os acentuos, troca espaço por hifens e cria sua imagem na tela.
@@ -6,25 +9,35 @@ nomeItem.forEach(item => {
     position = 0
     temp = removerAcentosEAdicionarHifen(item.innerHTML)
     itensMenu.push(temp.toLocaleLowerCase())
-    console.log(itensMenu)
-    criaImagem(item,temp.toLocaleLowerCase())
-    position ++
+    criaImagem(item, temp.toLocaleLowerCase())
+    position++
 })
 
 
 //Função para criar imagens e exibir na tela de acordo com o nome do item.
 // Por enquanto só funciona com a versão desktop das imagens.
-function criaImagem(item, nomeItem){
+function criaImagem(item, nomeItem) {
     let img = document.createElement('img')
     img.src = `assets/images/image-${nomeItem}-desktop.jpg`
-    item.append(img)
+    img.classList.add('menu__item_image')
+    item.insertAdjacentElement('afterbegin', img)
 }
 
 
 // Regex para remover acentuos e adicionar hifens ao espaçamento
 function removerAcentosEAdicionarHifen(texto) {
     return texto
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "") 
-      .replace(/\s+/g, "-");           
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .replace(/\s+/g, "-");
+}
+
+function trocaImgCarrinho() {
+    let img = document.createElement('img')
+    img.src = 'assets/images/illustration-empty-cart.svg'
+    textoCarrinhoVazio.insertAdjacentElement('beforebegin', img)
+}
+
+if(itensNoCarrinho.innerText == 0){
+    trocaImgCarrinho()
 }
